e0c146cf9f54bcbc8c29390e333a3a98c57b5b80
R-T <csri16001@gmail.com>
2022-01-18 17:55:59 +0900

    style format

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,26 +1,27 @@
-import React from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Button,
-  TouchableOpacity,
-  Image,
-  ImageBackground,
-  SafeAreaView,
-  StatusBar,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React from 'react'
+import { Text, View, StyleSheet, Button, TouchableOpacity, Image, ImageBackground, SafeAreaView, StatusBar } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 // メニュー画面
 export default function MenuScreen() {
-  const navigation = useNavigation();
-
-  return (
-    <View>
-      <Text>aiueo</Text>
-    </View>
-  );
+    const navigation = useNavigation()
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar />
+            <View style={styles.container}>
+                <Text style={{ fontSize: 20 }}>aiueo</Text>
+                <Button title="BARCODE" />
+            </View>
+        </SafeAreaView>
+    )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
